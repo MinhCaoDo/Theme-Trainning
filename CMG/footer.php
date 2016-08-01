4529199c67ffa9878baebac990cffb3168b94898
minhdocao <minhdc2311@gmail.com>
2016-08-01 18:07:29 +0700

    Commit

--- CMG/footer.php
+++
@@ -1,9 +0,0 @@
-<footer id='footer'>
-	<div class="copyright">
-		© <?php echo date('Y'); ?> <?php bloginfo( 'sitename' ); ?>. <?php _e('All rights reserved', 'thachpham'); ?>. <?php _e('This website is proundly to use WordPress', 'thachpham'); ?>
-	</div>
-
-</div> <!--end #container -->
- <?php wp_footer(); ?>
-</body> <!--end body-->
-</html> <!--end html -->
